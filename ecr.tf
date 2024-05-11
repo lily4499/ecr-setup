28284d9af686f4cc8d504717303d2c7eafdc1dc3
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-05-11 17:15:48 -0400

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 
 # Create the ECR repository
-resource "aws_ecr_repository" "netflix-app" {
-  name = "amazon-ecr"
+resource "aws_ecr_repository" "lil_ecr_repo" {
+  name = "netflix-app"
   # Additional repository configurations can be added here if needed
 }
 
 # Output ECR repository URL
 output "ecr_repository_url" {
-  value = "${aws_ecr_repository.netflix-app.repository_url}"
+  value = "${aws_ecr_repository.lil_ecr_repo.repository_url}"
 }
